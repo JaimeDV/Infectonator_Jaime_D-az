02376b4b162ba54fb4ab9e6af3fd4e281bf024e0
Erudox <66876173+JaimeDV@users.noreply.github.com>
2023-04-21 13:37:54 +0200

    Test

--- Assets/DecisionSystem/Actions/ActionCallMethod.cs
+++ Assets/DecisionSystem/Actions/ActionCallMethod.cs
@@ -37,6 +37,7 @@ public class ActionCallMethod : IAction
 		else
 		{
 			target.SendMessage(message, SendMessageOptions.DontRequireReceiver);
+			
 		}
 	}
 }
---
+++ Assets/S_testMachines.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_testMachines : MonoBehaviour
+{
+  public void Mua()
+    {
+        Debug.Log("Smooch");
+    }
+}
